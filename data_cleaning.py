f8a4c64729a9ee1ab620b4cc1405f3c945921577
JohnnyHsieh1020 <46452717+JohnnyHsieh1020@users.noreply.github.com>
2021-02-09 16:58:14 +0800

    data cleaning-0209

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,7 +5,7 @@ df = pd.read_csv('glassdoor_jobs.csv')
 
 # Salary Estimate: keep the numbers only.
 df['hourly'] = df['Salary Estimate'].apply(
-    lambda x: 1 if 'per hour' in x.lower() else 0)
+    lambda x: 1 if 'per hour' in x.lower() else 0)  # 增加一個欄位來註記欄位「Salary Estimate」中有 "Per hour" 的紀錄
 
 df['employer_provided'] = df['Salary Estimate'].apply(
     lambda x: 1 if 'employer provided salary:' in x.lower() else 0)
@@ -16,11 +16,31 @@ salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0])  # 移除 ”（
 
 rm_KD = salary.apply(lambda x: x.replace(
     'K', '').replace('$', ''))  # 移除 “Ｋ” 及 “＄”
-print(salary)
-# Job Description: python etc.
+
+rm_HR = rm_KD.apply(lambda x: x.lower().replace(
+    'per hour', ''))  # 移除 “Per hour”
+# 簡單統計薪水（最小、最大、平均）
+df['min_salary'] = rm_HR.apply(lambda x: int(x.split('-')[0]))
+df['max_salary'] = rm_HR.apply(lambda x: int(x.split('-')[1]))
+df['avg_salary'] = (df.min_salary + df.max_salary) / 2
 
 # Company name: keep text only.
+df['Company'] = df.apply(lambda x: x['Company Name']
+                         if x['Rating'] < 0 else x['Company Name'][:-4], axis=1)  # 取企業名稱
 
 # Location: keep state.
+df = df[df['Location'] != 'Remote']
+df = df[df['Location'] != 'United States']
+df = df[df['Location'] != 'Maryland']
+df = df[df['Location'] != 'New Jersey']
+df = df[df['Location'] != 'Virginia']
+
+df['job_location'] = df['Location'].apply(
+    lambda x: x.split(',')[1])  # 取欄位「Location」中後兩個字元
+
+print(df.job_location.value_counts())  # 統計 location 個數
 
 # Founded: age of company.
+df['age'] = df.Founded.apply(lambda x: x if x < 1 else 2021 - x)  # 計算企業年齡
+
+# Job Description: python etc.
